88b0bd06abe0d9c092029cd3ba3887eb62aa4be9
Alberto J. Rubio <yebenes9@gmail.com>
2016-11-03 12:12:33 +0100

    Enable scripts mode in docker containers

--- src/docker/service/run.sh
+++ src/docker/service/run.sh
@@ -1,3 +1,7 @@
-find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
-cd /
-/${project.name}/bin/${project.name} $@
+if [ "$SCRIPTS_MODE" = true ] ; then
+    find ${project.name}/scripts -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
+else
+    find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
+    cd /
+    /${project.name}/bin/${project.name} $@
+fi
