a27d2245072810c16985c75b0725b23cc612837f
Alberto J. Rubio <yebenes9@gmail.com>
2016-11-04 15:45:17 +0100

    Change scripts folder in containers

--- src/docker/service/run.sh
+++ src/docker/service/run.sh
@@ -1,5 +1,5 @@
 if [ "$SCRIPTS_MODE" = true ] ; then
-    find ${project.name}/scripts -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
+    find scripts/${project.name} -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
 else
     find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
     cd /
