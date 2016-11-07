d7d555626e5798150066fd923219a78e7b5d7e1d
Alberto J. Rubio <yebenes9@gmail.com>
2016-11-07 09:25:47 +0100

    It's necessary copy the plugins also for scripts mode

--- src/docker/service/run.sh
+++ src/docker/service/run.sh
@@ -1,7 +1,7 @@
+find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
+cd /
 if [ "$SCRIPTS_MODE" = true ] ; then
     find scripts/${project.name} -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
 else
-    find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
-    cd /
     /${project.name}/bin/${project.name} $@
 fi
