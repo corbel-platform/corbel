8367135d888925c6f5c8b5147042d57be514b0c9
Alexander De Leon <me@alexdeleon.name>
2017-01-18 22:43:25 +0100

    Find scripts in subdirectories

--- src/docker/service/run.sh
+++ src/docker/service/run.sh
@@ -1,7 +1,7 @@
 find ${project.name}/plugins -name "*.jar" -type f -exec cp '{}' ${project.name}/plugins ';'
 cd /
 if [ "$SCRIPTS_MODE" = true ] ; then
-    find scripts/${project.name} -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
+    find scripts -regex '.*/${project.name}/.*\.groovy' -type f -exec /${project.name}/bin/${project.name} cli '{}' ';'
 else
     /${project.name}/bin/${project.name} $@
 fi
